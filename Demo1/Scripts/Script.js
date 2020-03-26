7458e748327ab9379acb6e6dba7fc64df6db8021
96278 <96278@DESKTOP-P6JHFNO>
2020-03-26 20:35:43 +0200

    Display data on 2 ways (List and Table) using ng include directive in AngularJS

--- Demo1/Scripts/Script.js
+++ Demo1/Scripts/Script.js
@@ -1,17 +1,20 @@
+/// <reference path="angular.min.js" />
+
 var app = angular
     .module("myModule", [])
     .controller("myController", function ($scope) {
 
         var employees = [
-            { name: "Ben", dateOfbirth: new Date("November 23, 1980"), gender: "Male", salary: 55000, city: "London" },
-            { name: "Sara", dateOfbirth: new Date("May 05, 1970"), gender: "Female", salary: 68000, city: "Chennai"  },
-            { name: "Mark", dateOfbirth: new Date("August 15, 1974"), gender: "Male", salary: 57000, city: "London"  },
-            { name: "Pam", dateOfbirth: new Date("October 27, 1979"), gender: "Female", salary: 53000, city: "Chennai"  },
-            { name: "Todd", dateOfbirth: new Date("December 30, 1983"), gender: "Male", salary: 60000, city: "Chennai"  }
+            { name: "Ben", dateOfbirth: new Date("November 23, 1980"), gender: 1, salary: 55000, city: "London" },
+            { name: "Sara", dateOfbirth: new Date("May 05, 1970"), gender: 2, salary: 68000, city: "Chennai"  },
+            { name: "Mark", dateOfbirth: new Date("August 15, 1974"), gender: 1, salary: 57000, city: "London"  },
+            { name: "Pam", dateOfbirth: new Date("October 27, 1979"), gender: 1, salary: 53000, city: "Chennai"  },
+            { name: "Todd", dateOfbirth: new Date("December 30, 1983"), gender: 3, salary: 60000, city: "Chennai"  }
         ];
 
         $scope.employees = employees;
-        $scope.rowlimit = 3;
+        $scope.employeeView  = "EmployeeTable.html";
+        $scope.rowlimit = 5;
         $scope.sortColumn = "name";
         $scope.reverseSort = false;
         //****************Sorting column by click on column header**********************//
