6903389c5061d48f8ace239b986cbec67bcb8e8f
96278 <96278@DESKTOP-P6JHFNO>
2020-03-26 17:27:11 +0200

    Single search textbox, and is used to search multiple prop

--- Demo1/Scripts/Script.js
+++ Demo1/Scripts/Script.js
@@ -3,18 +3,18 @@ var app = angular
     .controller("myController", function ($scope) {
 
         var employees = [
-            { name: "Ben", dateOfbirth: new Date("November 23, 1980"), gender: "Male", salary: 55000 },
-            { name: "Sara", dateOfbirth: new Date("May 05, 1970"), gender: "Female", salary: 68000 },
-            { name: "Mark", dateOfbirth: new Date("August 15, 1974"), gender: "Male", salary: 57000 },
-            { name: "Pam", dateOfbirth: new Date("October 27, 1979"), gender: "Female", salary: 53000 },
-            { name: "Todd", dateOfbirth: new Date("December 30, 1983"), gender: "Male", salary: 60000 }
+            { name: "Ben", dateOfbirth: new Date("November 23, 1980"), gender: "Male", salary: 55000, city: "London" },
+            { name: "Sara", dateOfbirth: new Date("May 05, 1970"), gender: "Female", salary: 68000, city: "Chennai"  },
+            { name: "Mark", dateOfbirth: new Date("August 15, 1974"), gender: "Male", salary: 57000, city: "London"  },
+            { name: "Pam", dateOfbirth: new Date("October 27, 1979"), gender: "Female", salary: 53000, city: "Chennai"  },
+            { name: "Todd", dateOfbirth: new Date("December 30, 1983"), gender: "Male", salary: 60000, city: "Chennai"  }
         ];
 
         $scope.employees = employees;
         $scope.rowlimit = 3;
         $scope.sortColumn = "name";
         $scope.reverseSort = false;
-
+        //****************Sorting column by click on column header**********************//
         $scope.sortData = function (column) {
             $scope.reverseSort = ($scope.sortColumn == column) ? !$scope.reverseSort : false;
             $scope.sortColumn = column;
@@ -31,5 +31,18 @@ var app = angular
             return '';
         }
       
+        //****************Filtering  table by multiple properties - name and city**********************//
+        $scope.search = function (item) {
+            if ($scope.searchText == undefined) {
+                return true;
+            }
+            else {
+                if (item.city.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1 ||
+                    item.name.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1) {
+                    return true;
+                }
+            }
 
+            return false;
+        }
     });
