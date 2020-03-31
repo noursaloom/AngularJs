ef4ff03693d5a7ab42cf95a9f19b21077868b9cc
96278 <96278@DESKTOP-P6JHFNO>
2020-03-31 15:15:02 +0300

    Routing in angularjs

---
+++ Demo3/Scripts/script.js
@@ -0,0 +1,30 @@
+﻿var app = angular
+    .module("Demo", ["ngRoute"])
+    .config(function ($routeProvider,$locationProvider) {
+        $routeProvider
+            .when("home", {
+                templateUrl: "Templates/home.html",
+                controller: "homeController"
+            })
+            .when("courses", {
+                templateUrl: "Templates/courses.html",
+                controller: "coursesController"
+            })
+            .when("students", {
+                templateUrl: "Templates/students.html",
+                controller: "studentsController"
+            });
+        $locationProvider.html5Mode(true);
+    })
+    .controller("homeController", function ($scope) {
+        $scope.message = "Home Page";
+    })
+    .controller("coursesController", function ($scope) {
+        $scope.courses = ["C#", "VB.NET", "ASP.NET", "SQL Server"];
+    })
+    .controller("studentsController", function ($scope, $http) {
+        $http.get("StudentService.asmx/GetAllStudents")
+            .then(function (response) {
+                $scope.students = response.data;
+            })
+    })
---
+++ Demo3/student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo3
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string gender { get; set; }
+        public string city { get; set; }
+
+    }
+}
